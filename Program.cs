3f0871e781dfe82d61b9d9a80d02694bc9ab5678
Brad Stell <bradstell87@gmail.com>
2018-02-18 14:42:38 -0500

    Formatting output, TODO - bring algorithms out of graph

--- Program.cs
+++ Program.cs
@@ -13,31 +13,31 @@ namespace GraphTraversal
             buildGraphFromInitialState(graph, rawGraphData);
 
             // Question 1: The distance of the route A-B-C
-            Console.WriteLine($" The distance of A-B-C => {CalculatePathWeight(graph, "A-B-C")}");
+            Console.WriteLine($" The distance of A-B-C \t\t | {CalculatePathWeight(graph, "A-B-C")}");
 
             // Question 2: The distance of the route A-D
-            Console.WriteLine($" The distance of A-D => {CalculatePathWeight(graph, "A-D")}");
+            Console.WriteLine($" The distance of A-D \t\t | {CalculatePathWeight(graph, "A-D")}");
 
             // Question 3: The distance of the route A-D-C
-            Console.WriteLine($" The distance of A-D-C => {CalculatePathWeight(graph, "A-D-C")}");
+            Console.WriteLine($" The distance of A-D-C \t\t | {CalculatePathWeight(graph, "A-D-C")}");
 
             // Question 4: The distance of the route A-E-B-C-D
-            Console.WriteLine($" The distance of A-E-B-C-D => {CalculatePathWeight(graph, "A-E-B-C-D")}");
+            Console.WriteLine($" The distance of A-E-B-C-D \t | {CalculatePathWeight(graph, "A-E-B-C-D")}");
 
             // Question 5: The distance of the route A-E-D
-            Console.WriteLine($" The distance of A-E-D => {CalculatePathWeight(graph, "A-E-D")}");
+            Console.WriteLine($" The distance of A-E-D \t\t | {CalculatePathWeight(graph, "A-E-D")}");
 
             // Question 6: # of trips starting at C and ending at C with <= 3 stops
-            Console.WriteLine($"# of trips from C to C => {graph.NumberOfTripsBetweenVerticiesLessThanStops('C', 'C', 3)}");
+            Console.WriteLine($" # of trips from C to C \t | {graph.NumberOfTripsBetweenVerticiesLessThanStops('C', 'C', 3)}");
 
             // Question 7: # of trips starting at A and ending at C with <= 4 stops
-            Console.WriteLine($"# of trips from A to C => {graph.NumberOfTripsBetweenVerticiesWithExactStops('A', 'C', 4)}");
+            Console.WriteLine($" # of trips from A to C \t | {graph.NumberOfTripsBetweenVerticiesWithExactStops('A', 'C', 4)}");
 
             // Question 8: Shortest path from A-C
-            Console.WriteLine($"Shortest path from A to C => {graph.ShortestPath('A', 'C')}");
+            Console.WriteLine($" Shortest path from A to C \t | {graph.ShortestPath('A', 'C')}");
 
             // Question 8: Shortest path from B-B
-            Console.WriteLine($"Shortest path from B to B => {graph.ShortestPath('B', 'B')}");
+            Console.WriteLine($"Shortest path from B to B \t | {graph.ShortestPath('B', 'B')}");
 
             // Question 9: # of different routes from C to C with distance less than 30
             Console.WriteLine($"# of trips from C to C with distance less than 30 => {graph.NumberOfTripsBetweenVerticiesLessThanDistance('C', 'C', 30)}");
